b45d5270167483f06579a69cf13f5d88f80a38bb
avishnyakov <anton.vishnyakov@gmail.com>
2014-05-28 09:45:14 +1000

    + base project structure

---
+++ SPMeta2.Contoso/SPMeta2.Contoso.Model/Consts/CntsConsts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Contoso.Model.Consts
+{
+    public static class CntsConsts
+    {
+        #region properties
+
+        public const string DefaultFieldGroup = "SPMeta2.Contoso";
+        public const string DefaultContentTypeGroup = "SPMeta2.Contoso";
+
+        #endregion
+    }
+}
---
+++ SPMeta2.Contoso/SPMeta2.Contoso.Model/Definitions/CntsListsViews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Contoso.Model.Consts;
+using SPMeta2.Definitions;
+using SPMeta2.Enumerations;
+
+namespace SPMeta2.Contoso.Model.Definitions
+{
+    public static class CntsListsViews
+    {
+        #region properties
+
+
+
+        #endregion
+    }
+}
